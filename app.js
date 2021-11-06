1bbc1090871120f2d795eac83da6f172bf9a22e4
jean <jeandeveweb@gmail.com>
2021-11-06 15:08:36 +0100

    projet complet

---
+++ app.js
@@ -0,0 +1,32 @@
+const btns = document.querySelectorAll(".btn-modale");
+const modaleEquipement = document.querySelector(".bloc-modale");
+const fermetureModale = document.querySelector(".fermeture-modale");
+const imgIndex = document.querySelector(".bloc-modale img");
+
+if (window.matchMedia("(min-width: 850px)").matches) {
+  btns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      imgIndex.src = `./ressources/img${e.target.getAttribute(
+        "data-index"
+      )}-equip.jpg`;
+      modaleEquipement.classList.add("active-modale");
+    });
+  });
+
+  modaleEquipement.addEventListener("click", () => {
+    modaleEquipement.classList.remove("active-modale");
+  });
+}
+
+//  ANIM NAVBAR
+
+const nav = document.querySelector("nav");
+
+window.addEventListener('scroll', () => {
+
+    if(window.scrollY > 30){
+        nav.className = 'anim-nav';
+    } else {
+        nav.className = '';
+    }
+})
